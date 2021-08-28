73724509522574bd35f8d0b25e5377a10a6e80d4
Jeff Doyle <shelleydoyle@eastlink.ca>
2021-08-28 01:47:14 -0300

    main: id_tag_update no longer required

--- source/blender/blenkernel/BKE_main.h
+++ source/blender/blenkernel/BKE_main.h
@@ -100,8 +100,6 @@ typedef struct Main {
 	ListBase wm;
 	ListBase cachefiles;
 
-	char id_tag_update[MAX_LIBARRAY];
-
 	/* Must be generated, used and freed by same code - never assume this is valid data unless you know
 	 * when, who and how it was created.
 	 * Used by code doing a lot of remapping etc. at once to speed things up. */
